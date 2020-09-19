ee72a3d3c02f2c4e59247d087657dd5bd728ab42
Dillon Rice <drice89@gmail.com>
2020-09-18 17:11:45 -0700

    add styles

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ npm start
 
 navigate to localhost:8080/?file={your-file-id-here}
 
+
+Your project should have JSS installed -> styles are written in jss
+
--- app.js
+++ app.js
@@ -6,23 +6,24 @@ const componentProcessor = require('./lib/component-processor.js').componentProc
 const file = 'SxVUDbkV2O49lpsgyyF7Cm'
 
 const parseQuery = (req, res) => {
-  console.log(req)
+  //console.log(req)
   res.writeHead(200, {'Content-Type': 'text/html'});
   let q = url.parse(req.url, true).query
   let fileLocation = q.file
   const client = figma.Client({
     personalAccessToken: config.apiKey
   })
-
-  client.file(fileLocation)
-    .then(( { data }) => {
-      const rectangle = data.document.children[0].children[0]
-      componentProcessor(rectangle)
-      console.log('hello')
-      //console.log(rectangle)
-      //res.end(JSON.stringify(data.document))
-    })
-    .catch(console.log)
+  if(fileLocation) {
+    console.log(q)
+    client.file(fileLocation)
+      .then(( { data }) => {
+        const rectangle = data.document.children[0].children[0]
+        componentProcessor(rectangle)
+        //console.log(rectangle)
+        //res.end(JSON.stringify(data.document))
+      })
+      .catch(console.log)
+  }
     
 }
 
